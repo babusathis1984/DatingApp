10bf6bbdef32b4e23dec91459ca247a1d25f9d57
Sathish Babu <babusathis@gmail.com>
2020-05-30 00:19:57 +0530

    test

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -63,7 +63,7 @@ namespace DatingApp.API.Data
             if(await _context.Users.AnyAsync(x=>x.UserName== username))
                 return true;
                 
-            return false;
+            return false;;
         }
     }
 }
